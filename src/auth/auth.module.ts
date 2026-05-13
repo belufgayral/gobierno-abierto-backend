e2f68105dbfb50279162229343a2e4989bd80d0c
Ulises <ulisestcortes@gmail.com>
2026-05-13 15:05:46 -0300

    Cambio en el module.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,13 +4,19 @@ import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt/jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UserModule,
-    JwtModule.register({
-      secret: process.env.SECRET_KEY_JWT, // Debería venir de una variable de entorno
-      signOptions: { expiresIn: '1h' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        // Usamos el configService para asegurarnos de que el .env esté cargado
+        secret: configService.get<string>('SECRET_KEY_JWT'), 
+        signOptions: { expiresIn: '1h' },
+      }),
     }),
   ],
   controllers: [AuthController],
