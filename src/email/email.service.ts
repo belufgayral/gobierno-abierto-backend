d7780517873f3fc78d5837f63f753cffa9978180
Ulises <ulisestcortes@gmail.com>
2026-05-18 16:40:11 -0300

    Cambios en el componente email.

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -20,17 +20,16 @@ export class EmailService {
         });
     }
 
-    async sendContact(name: string, message: string) {
-        const emailRecepcion = process.env.RECEIVER_EMAIL; // El mail que recibe
+    async sendContact(name: string, message: string, dni: string, contacto: string) {
+        const emailRecepcion = process.env.RECEIVER_EMAIL;
         if (!emailRecepcion) throw new Error("No hay email de recepción.");
 
         try {
-            // .sendMail() reemplaza a .emails.send()
             await this.transporter.sendMail({
                 // El "from" ahora debe ser generalmente el usuario del SMTP
                 from: `"Portal Abierto Lobería" <${process.env.SMTP_USER}>`,
                 to: emailRecepcion,
-                subject: `Nuevo mensaje de contacto — ${name}`,
+                subject: `Nuevo mensaje de contacto — ${name}, dni — ${dni}`,
                 html: `
                     <div style="font-family: sans-serif; max-width: 600px; margin: 0 auto; color: #333;">
                         <h2 style="color: #4C9734; border-bottom: 1px solid #badabb; padding-bottom: 12px;">
@@ -41,6 +40,10 @@ export class EmailService {
                             <td style="padding: 8px 0; font-weight: bold; width: 120px; color: #4C9734;">Nombre:</td>
                             <td style="padding: 8px 0;">${name || 'No informado'}</td>
                         </tr>
+                        <tr>
+                            <td style="padding: 8px 0; font-weight: bold; width: 120px; color: #4C9734;">Contacto:</td>
+                            <td style="padding: 8px 0;">${contacto}</td>
+                        </tr>
                         </table>
                         <div style="background:rgb(246, 251, 246); border-left: 3px solid #4C9734; padding: 16px 20px; border-radius: 0 4px 4px 0;">
                         <p style="margin: 0; white-space: pre-wrap;">${message}</p>
@@ -50,7 +53,7 @@ export class EmailService {
             });
             return { success: true };
         } catch (error) {
-            console.error(error); // Siempre es bueno loguear el error real de SMTP
+            console.error(error); 
             throw new Error('Error al enviar el email');
         }
     }
